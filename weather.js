2659d6dfedd6961ffcfd3a9b6b9a2ff157ccb424
Johan Toft <johan.toft@gmail.com>
2017-05-06 10:21:08 +0000

    Mapping of weather codes

--- weather.js
+++ weather.js
@@ -17,15 +17,42 @@ function init_weather(apikey)
   gothenburg_today_request =  "http://api.openweathermap.org/data/2.5/weather?id=2711533&appid=" + apikey
   request(gothenburg_today_request, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-        weather_today = body;
+        weather_today = JSON.parse(body);
      }
     })
 }
 
 
+
+//maps api weather to our tags
+function map_weather(weather_id)
+{
+  var major_weather_code = Math.floor(weather_id/100);
+  switch(major_weather_code){
+    case 2: return ["Rainy"] 
+     break;
+    case 3: return ["Rainy"] 
+     break;
+    case 5: return ["Rainy"] 
+     break;
+    case 6: return ["Snowy"]
+     break;
+    case 8: if(weather_id <= 801){
+              return ["Sunny"]
+            } else {
+              return ["Cloudy"]
+            }
+     break;
+    case 9: return ["Rainy"]
+     break;
+    default: return ["Unknowny"]
+     break;
+  }
+}
+
 function today()
 {
-  return weather_today;
+  return map_weather(weather_today.weather[0].id);
 }
 
 exports.today = today; 
