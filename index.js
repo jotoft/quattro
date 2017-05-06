8fe5d4249e56dc4da435afd5c0dfa570e243c7e4
Johan Toft <johan.toft@gmail.com>
2017-05-06 09:02:23 +0000

    clean up index.js

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
 var express = require('express')
 var request = require('request')
 
-var fs = require('fs')
-
 var weather = require('./weather')
-
-/*
-fs.readFile('api_key', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
-  var apikey = data;
-  init_weather(apikey);
-});
-
-var weather_today;
-
-function init_weather(apikey)
-{
-  gothenburg_today_request =  "http://api.openweathermap.org/data/2.5/weather?id=2711533&appid=" + apikey
-  request(gothenburg_today_request, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        weather_today = body;
-     }
-    })
-}
-*/
 
 var app = express()
 
